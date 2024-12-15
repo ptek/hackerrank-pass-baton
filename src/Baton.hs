5c0a8d2406ae20b8d4ec78e7c08322f76f65d610
Pavlo Kerestey <pavlo@kerestey.net>
2024-12-15 22:37:31 +0100

    Improve variable naming, Explain reasoning in comments

--- src/Baton.hs
+++ src/Baton.hs
@@ -11,27 +11,43 @@ data Direction = Forward | Back | PassingBackToBeginning deriving (Eq, Ord, Show
 
 
 passBaton :: Int -> Int -> (Int, Int)
-passBaton friends _ | friends <= 0 = (0, 0)
-passBaton _ time | time <= 0 = (0, 0)
+passBaton friends time | friends <= 0 || time <= 0 = (0, 0)
 passBaton 1 _ = (1, 1)
-passBaton friends time = (starting_point, ending_point)
+passBaton friends time = (passer, receiver)
  where
+  -- the sequence is the whole set of passes of baton first forward, then backwards
+  -- and then back to the first friend
   position_in_sequence :: Int = time `mod` (friends + friends - 2)
+
   -- time is always positive, because the case of 0 is pattern matched against
   -- in the previous checks. Therefore, when position_in_sequence is 0, it means
-  -- that we are passing back to the beginning
-  which_direction_start :: Direction =
+  -- that we are at the end of the sequence and the batton is being passed back
+  -- to the first friend
+  which_direction_passer :: Direction =
     if position_in_sequence == 0
       then PassingBackToBeginning
       else
         if position_in_sequence < friends
           then Forward
           else Back
-  which_direction_end :: Direction = if position_in_sequence + 1 < friends then Forward else Back
-  starting_point
-    | which_direction_start == PassingBackToBeginning = 2
-    | which_direction_start == Forward = position_in_sequence
-    | otherwise = friends * 2 - position_in_sequence
-  ending_point
-    | which_direction_end == Forward = position_in_sequence + 1
-    | otherwise = friends * 2 - position_in_sequence - 1
+
+  -- The direction of the receiver turns backwards when the baton reaches the
+  -- middle of the sequence. As the sequence is forwards, then backwards, the
+  -- middle will be when the baton passes all the friends
+  which_direction_receiver :: Direction =
+    if position_in_sequence == 0
+      then PassingBackToBeginning
+      else
+        if position_in_sequence + 1 < friends
+          then Forward
+          else Back
+
+  passer = case which_direction_passer of
+    PassingBackToBeginning -> 2
+    Forward -> position_in_sequence
+    Back -> friends * 2 - position_in_sequence
+
+  receiver = case which_direction_receiver of
+    PassingBackToBeginning -> 1
+    Forward -> position_in_sequence + 1
+    _ -> friends * 2 - position_in_sequence - 1
